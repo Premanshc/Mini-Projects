7cdc4552b462223c2fc3fac92c9ef58b4f4ef020
Premansh Chanderiya <97906920+Premanshc@users.noreply.github.com>
2023-04-18 00:36:03 +0530

    add drum kit project to mini project folder

---
+++ Drum Kit Web Project/index.js
@@ -0,0 +1,56 @@
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numberOfDrums;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",eventHandler);
+}
+
+function eventHandler(){
+    var buttonInnerHtml = this.innerHTML;
+    makeSound(buttonInnerHtml);
+    }
+
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+});
+
+function makeSound(key){
+    switch (key){
+        case "a":
+            var kickBass = new Audio('./sounds/kick-bass.mp3');
+            kickBass.play();
+            break;
+            
+        case "w":
+            var snare = new Audio('./sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case "l":
+            var tom4 = new Audio('./sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        
+        case "k":
+            var tom3 = new Audio('./sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        
+        case "d":
+            var tom1 = new Audio('./sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        
+        case "j":
+            var tom2 = new Audio('./sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        
+        case "s":
+            var crash = new Audio('./sounds/crash.mp3');
+            crash.play();
+            break;
+
+        default:
+            console.log("Wrong button entry!!");
+    }
+}
